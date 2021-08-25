484efd86f821a8212fa27d480433ae27d2709d28
Talha Malik <talha@blendable.ca>
2021-08-25 10:53:39 -0400

    Add import to make library compatible with django 3

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
     install_requires=[
         'Django>=1.11',
         'django-appconf',
+        'six',
     ],
     packages=find_packages(exclude=['tests*']),
     include_package_data=True,
